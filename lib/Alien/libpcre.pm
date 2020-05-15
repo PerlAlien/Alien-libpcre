4ede2b74a9f0d68144967c55d0cadd41630d2049
Graham Ollis <plicease@cpan.org>
2020-05-15 05:50:26 -0600

    initial implementation

---
+++ lib/Alien/libpcre.pm
@@ -0,0 +1,22 @@
+package Alien::libpcre;
+
+use strict;
+use warnings;
+use 5.008001;
+use base qw( Alien::Base );
+
+# ABSTRACT: Find or download and install libpcre (The "Perl Compatible" Regular Expressions library)
+# VERSION
+# ALIEN SYNOPSIS
+# ALIEN DESCRIPTION
+# ALIEN SEE ALSO
+
+=head1 CAVEATS
+
+The pcre, is not, as the name might suggest, actually compatible with modern Perl regular expressions.
+
+=cut
+
+1;
+
+
