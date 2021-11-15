3464ca2a27f26b292c1cc7d8b13eb28686ef6a8c
Graham Ollis <plicease@cpan.org>
2021-11-15 08:33:09 -0700

    Documentation should make it clear that this is for PCRE1

--- lib/Alien/libpcre.pm
+++ lib/Alien/libpcre.pm
@@ -8,8 +8,22 @@ use base qw( Alien::Base );
 # ABSTRACT: Find or download and install libpcre (The "Perl Compatible" Regular Expressions library)
 # VERSION
 # ALIEN SYNOPSIS
-# ALIEN DESCRIPTION
 # ALIEN SEE ALSO
+
+=head1 DESCRIPTION
+
+B<Note> that this L<Alien> is for PCRE1, the deprecated older version of the
+Perl (in)Compatible Regular Expression (PCRE) library.  It exists for projects
+that require the older version of PCRE.  Currently the author uses it for
+source builds of L<Alien::libnewrelic> and L<NewFangle>.  If you need the
+newer currently supported version of PCRE see L<Alien::PCRE2>.
+
+This distribution provides PCRE1 so that it can be used by other
+Perl distributions that are on CPAN.  It does this by first trying to
+detect an existing install of pcre on your system.  If found it
+will use that.  If it cannot be found, the source code will be downloaded
+from the internet and it will be installed in a private share location
+for the use of other modules.
 
 =head1 CAVEATS
 
