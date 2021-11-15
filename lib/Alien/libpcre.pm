b5e22201aaadf76817608a820dce02f834f4e357
Graham Ollis <plicease@cpan.org>
2021-11-15 08:40:45 -0700

    add caveat

--- lib/Alien/libpcre.pm
+++ lib/Alien/libpcre.pm
@@ -29,6 +29,10 @@ for the use of other modules.
 
 The pcre, is not, as the name might suggest, actually compatible with modern Perl regular expressions.
 
+On Debian based systems (including Ubuntu), the development package for PCRE1 is C<libpcre3-dev>, which
+is quite confusing, but I am sure there was a good reason for it.  If you want to do a C<system> install
+use that package not the confusingly newer C<libpcre2-dev>.
+
 =cut
 
 1;
